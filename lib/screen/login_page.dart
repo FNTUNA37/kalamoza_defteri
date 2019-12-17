ae09ece5c9b9a12afb54cc312bc0d8e3889b1518
FNTUNA37 <48826801+FNTUNA37@users.noreply.github.com>
2019-12-17 21:42:41 +0300

    loginpage

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
-
-const users = const {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-};
+import 'package:kalamoza_defteri/authentication.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -12,38 +8,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  Duration get loginTime => Duration(milliseconds: 2250);
-
-  Future<String> _authUser(LoginData data) {
-    print('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'Username not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
-  }
-
-  Future<String> _recoverPassword(String name) {
-    print('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'Username not exists';
-      }
-      return null;
-    });
-  }
+  Auth auth = Auth();
+  LoginData data;
 
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'FURKAN',
-      onLogin: _authUser,
-      onSignup: _authUser,
-      onRecoverPassword: _recoverPassword,
+      onLogin: (_) => auth.signIn(data),
+      onSignup: (_) => auth.signUp(data),
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => LoginPage(),
